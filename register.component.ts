0acda9ea61f26726c9ba31330a48d9bf59dbb887
sahana <sahnasbegam.mahaboobasha@citiustech.com>
2021-12-15 19:02:39 +0530

    5th commit in dev at 7/01PM 15/12

---
+++ register.component.ts
@@ -0,0 +1,10 @@
+//Component- decorative
+import {Component} from '@angular/core';
+@Component({
+    selector:'student.root',
+    templateUrl:'./student.component.html'
+    styleUrls:templateUrl:['./student.component.css']
+})
+class StudentComponent{
+
+}
